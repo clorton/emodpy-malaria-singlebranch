5cec89d9b33649fa95a0404d5a6de39d8a0ebba4
Sam Buxton <67336501+SBuxton-IDM@users.noreply.github.com>
2020-12-10 18:05:44 -0800

    Added import tests (#19)
    * Added import tests
    
    * Added test for malaria demographics
    
    * Split interventions into multiple tests
    
    * Delete test_import.cpython-38.pyc
    
    * Delete test_imports.cpython-38.pyc

---
+++ tests/unittests/test_import.py
@@ -0,0 +1,51 @@
+import unittest
+
+class MalariaTestImports(unittest.TestCase):
+    def setUp(self) -> None:
+        self.expected_items = None
+        self.found_items = None
+        pass
+
+    def verify_expected_items_present(self, namespace):
+        self.found_items = dir(namespace)
+        for item in self.expected_items:
+            self.assertIn(
+                item,
+                self.found_items
+            )
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_requirements(self):
+        import emod_api
+        import emodpy_malaria
+        import emodpy
+        # Testing that we can import all requirements
+        checks = [dir(package) for package in [emod_api, emodpy_malaria, emodpy]]
+        for package in checks:
+            self.assertIn('__package__', package)
+
+    def test_intervention_bednet(self):
+        from emodpy_malaria import interventions
+        from emodpy_malaria.interventions import bednet
+
+        self.expected_items = [
+            "Bednet"
+        ]
+        self.verify_expected_items_present(namespace=bednet)
+
+    def test_intervention_sugartrap(self):
+        from emodpy_malaria.interventions import sugartrap
+        self.expected_items = [
+            "SugarTrap"
+        ]
+        self.verify_expected_items_present(namespace=sugartrap)
+
+    def test_demographics_imports_emodapi(self):
+        import emodpy_malaria.demographics.MalariaDemographics as Demographics
+
+        self.expected_items = [
+            "DT", "Demographics", "Node", "fromBasicNode"
+        ]
+        self.verify_expected_items_present(namespace=Demographics)
