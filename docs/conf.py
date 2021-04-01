a28d15a062c4a4aa174b91dca5aba23155b9410d
RCarter-IDM <35471720+RCarter-IDM@users.noreply.github.com>
2021-04-01 14:09:41 -0700

    install, sphinx copy button and containers (#122)
    * install, sphinx copy button and containers
    
    * Update installation-basic.rst
    
    * vpn warning to the top
    
    Co-authored-by: Jonathan Bloedow <jonathanhhb@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -198,9 +198,11 @@ html_static_path = ['_static']
 
 html_context = {
     'css_files': [
-        '_static/theme_overrides.css'
+        '_static/theme_overrides.css',
+        '_static/copy_button.css'
         ]
 }
+html_js_files = ['show_block_by_os.js']
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the docs.
