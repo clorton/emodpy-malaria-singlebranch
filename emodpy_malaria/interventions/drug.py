db46cd846ae92715732acce0a3f410c595e2536b
JSchripsema-IDM <jschripsema@idmod.org>
2021-04-30 19:03:18 -0600

    edited docstrings for drug.py interventions (#141)
    * edited docstrings for drug.py interventions
    
    * added more detail to drug_name docstring

--- emodpy_malaria/interventions/drug.py
+++ emodpy_malaria/interventions/drug.py
@@ -1,3 +1,7 @@
+"""
+This module contains functionality for defining antimalarial drug interventions.
+"""
+
 from emod_api import schema_to_class as s2c
 from emod_api.interventions import utils
 import json
@@ -12,7 +16,21 @@ def AntiMalarialDrug(
         node_ids=None
     ):
     """
-    AntiMalarialDrug intervention wrapper.
+    Add an antimalarial drug intervention to your campaign. This is equivalent to 
+    :doc:`emod-malaria:parameter-campaign-individual-antimalarialdrug`.
+
+    Args:
+        camp: The :py:obj:`emod_api:emod_api.campaign` object to which the intervention will be added. 
+        start_day: The day of the simulation on which the drug is distributed. We recommend 
+            aligning this with the start of the simulation. 
+        coverage: The proportion of the population that will receive the drug.
+        drug_name: The name of the drug to distribute in a drug intervention. Possible values are 
+            contained in **Malaria_Drug_Params** in :doc:`emod-malaria:parameter-configuration-drugs`.
+            Use :py:meth:`~emodpy_malaria.config.set_team_drug_params` to set those values.
+        node_ids: The IDs of the nodes in which to distribute the drug.
+
+    Returns:
+        The intervention event.
     """
     schema_path = camp.schema_path
     # First, get the objects
@@ -45,6 +63,19 @@ def AntiMalarialDrug(
 
 
 def new_intervention_as_file( camp, start_day, filename=None ):
+    """
+    Take an :doc:`emod-malaria:parameter-campaign-individual-antimalarialdrug`
+    intervention from a JSON file and add it to your campaign.
+
+    Args:
+        camp: The :py:obj:`emod_api:emod_api.campaign` object to which the intervention will be added. 
+        start_day: The day of the simulation on which the drug is distributed. We recommend 
+            aligning this with the start of the simulation.
+        filename: The JSON file that contains the intervention.
+
+    Returns:
+        The filename.
+    """
     campaign = {}
     campaign["Events"] = []
     campaign["Events"].append( AntiMalarialDrug( camp, start_day ) )
